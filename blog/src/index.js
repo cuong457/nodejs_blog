19daa671d9bc0d2bfd7762c2a5b5e62bbb5c7b7a
cuong457 <pnccuong20@clc.fitus.edu.vn>
2022-11-08 22:31:16 +0700

    Done Key-Knowledge (Part 2)

--- blog/src/index.js
+++ blog/src/index.js
@@ -8,8 +8,13 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engines
 app.engine('hbs', engine({
@@ -18,12 +23,21 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.get('/news', (req, res) => {
     res.render('news');
+});
+
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+
+app.post('/search', (req, res) => {
+    res.send('');
 });
 
 app.listen(port, () => {
